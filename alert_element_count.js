b5cf16bb5483e1245f68b9c9005958129664d2c0
BoomBoomMushroom <77977795+BoomBoomMushroom@users.noreply.github.com>
2021-12-02 14:14:09 -0500

    Update alert_element_count.js

--- alert_element_count.js
+++ alert_element_count.js
@@ -1,1 +1,2 @@
+// javascript:fetch("https://raw.githubusercontent.com/BoomBoomMushroom/inspect-element-script/main/alert_element_count.js").then((res) => res.text().then((t) => eval(eval(t))))
 unescape("%61%6C%65%72%74%28%64%6F%63%75%6D%65%6E%74%2E%67%65%74%45%6C%65%6D%65%6E%74%73%42%79%54%61%67%4E%61%6D%65%28%22%2A%22%29%2E%6C%65%6E%67%74%68%29")
